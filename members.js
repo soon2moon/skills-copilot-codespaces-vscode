03b994cb5bb7828c8dc7bf603599f2f8acb3482e
ucan <160035107+soon2moon@users.noreply.github.com>
2024-10-15 11:56:16 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillsMember() {
+    const members = [
+        {
+            name: "John Doe",
+            age: 25,
+            skills: ["Javascript", "React", "Node"]
+        },
+        {
+            name: "Jane Doe",
+            age: 26,
+            skills: ["HTML", "CSS", "SASS"]
+        }
+    ];
+
+    return members;
+}
